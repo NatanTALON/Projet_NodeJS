262eacd229fd52eeeb19084be1d1cea402348a3e
Natan <ntalon@enssat.fr>
2019-03-08 16:06:45 +0100

    finission

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const ejs = require('ejs')
 const app = express()
 const port = 3000
 
-const gameList = [{name: "Space Game", href: `http://localhost:${port}/SpaceGame`}];
+const gameList = [
+	{name: "Space Game", href: `/SpaceGame`, highscore: 0},
+	{name: "Random", href: `http://localhost:${port}/Random`, highscore: 0}
+];
+
+const user = "admin";		//a changer
 
 //select view engine
 app.set('view engine', 'ejs')
@@ -20,8 +25,12 @@ app.get('/GameSelection', function(req, res){
 	res.render('gameList', {gameList: gameList});
 })
 
-app.get('/SpaceGame', function(req, res){
+app.get('/SpaceGame', function(req, res){		//ne marche pas => trouve pas les sources
 	res.render('spaceGame', {user: user});
+})
+
+app.get('/Random', function(req, res){			//a faire
+	res.render('random', {user: user});
 })
 
 
