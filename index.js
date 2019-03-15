a2505a7d36f542777adea7a67f404dd5543af24b
Romane <rcastel@enssat.fr>
2019-03-15 11:58:48 +0100

    connection to dataBase mongodb

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/public', express.static(__dirname + '/public'));
 
+
+///////////////dataBase Connection //
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://RC:B4IgWhoqchuiTm3w@cluster0-uuws7.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+/////////////////////////////////////
 var gameList = [
 	{name: "Space Game", href: `/SpaceGame`, highscore: 0},
 	{name: "Random", href: `/Random`, highscore: 0}
@@ -19,20 +31,24 @@ var user = {
 };		//a changer
 
 
+//////////////select view engine///
+app.set('view engine', 'ejs')
+
+
 
 //////////////login////////////////
 app.get('/Login', function(req, res){
 	res.render('login', {user: user});
 })
 
-
-app.get('/Random', function(req, res){
-	res.render('random', {user: user});
+/////////////subscription//////////
+app.get('/Subscription', function(req, res){
+	res.render('subscription', {user: user});
 })
-//select view engine
-app.set('view engine', 'ejs')
 
 
+
+/////////////gameSelection/////////
 app.get('/GameSelection', function(req, res){
 	res.render('gameList', {gameList: gameList});
 })
@@ -42,11 +58,13 @@ app.post('/GameSelection', function(req, res){
 })
 
 
-
+/////////////spaceGame////////////
 app.get('/SpaceGame', function(req, res){		//ne marche pas => trouve pas les sources
 	res.render('spaceGame', {user: user});
 })
 
+
+/////////////random//////////////
 app.get('/Random', function(req, res){
 	res.render('random', {user: user});
 })
