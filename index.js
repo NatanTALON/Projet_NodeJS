9567661d615169f301b0c6a1fb54eebe4123b453
Romane <rcastel@enssat.fr>
2019-03-17 17:45:38 +0100

    ajout des modèles des tables pour la bdd

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://RC:B4IgWhoqchuiTm3w@cluster0-uuws7.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+  const collection = client.db("ProjetNode").collection("Users");
+  collection.createIndex( { "login":"String" }, function(err, result) {
+   console.log(result);
+});
   client.close();
 });
+
 
 /////////////////////////////////////
 var gameList = [
