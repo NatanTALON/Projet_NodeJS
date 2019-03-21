4e10eb1c20bc849382d0315882ae0fb369740505
Natan <ntalon@enssat.fr>
2019-03-21 11:55:09 +0100

    Utilisation d'express-session pour la sécurité du site

--- index.js
+++ index.js
@@ -1,57 +1,55 @@
-//functions used to check if user is valid
-var isRegistered = function(cookie) {
-	return cookie === user.name;
-};
-
-//require needed middlewares
+/////////////////// require needed middlewares /////////////////////
 const bodyParser = require('body-parser');
 const express = require('express');
 const ejs = require('ejs');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-//configure app to use express on port 3000
+
+
+////////// configure app to use express on port 3000///////////////
 const app = express();
 const port = 3000;
 
-//tell app to use imported middlewares
+//tell express where to find js sources for games
+app.use('/public', express.static(__dirname + '/public'));
+
+
+
+///////////// select view engine /////////////////////////
+app.set('view engine', 'ejs');
+
+
+
+/////////////// tell express to use middlewares ///////////////
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cookieParser());
 
-//creating middleware for session handling using cookies
-app.use(function (req, res, next) {
-	var cookie = req.cookies.login;
-	if ((req.url !== '/Login' && cookie === undefined) || !isRegistered(cookie)) {
+//session handling using cookies
+app.use(session({
+	secret: 'ojzfFv5zcHfD',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		maxAge: 4*60*60*1000,	//a session last 4 hours
+	}
+}));
+
+//middleware to check if the user is logged in
+app.use((req, res, next) => {
+	var url = req.url.split('/');
+	if (url.length < 2 || (url[1] === 'secure' && (!req.session || !req.session.user))) {
 		res.redirect('/Login');
 	} else {
 		next();
 	}
 });
 
-//tell express where to find js sources for games
-app.use('/public', express.static(__dirname + '/public'));
 
 
-//select view engine
-app.set('view engine', 'ejs')
-
-
-///////////////dataBase Connection ////////////
-/*const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://RC:B4IgWhoqchuiTm3w@cluster0-uuws7.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});*/
-
-/////////////////////////////////////
-
-
+/////////////// temporaire ///////////////////////
 var gameList = [
-	{name: "Space Game", href: `/SpaceGame`, highscore: 0},
-	{name: "Random", href: `/Random`, highscore: 0}
+	{name: "Space Game", href: '/secure/SpaceGame', highscore: 0},
+	{name: "Random", href: '/secure/Random', highscore: 0}
 ];
 
 var user = {
@@ -61,55 +59,57 @@ var user = {
 
 
 //////////////database connection////
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://RC:B4IgWhoqchuiTm3w@cluster0-uuws7.mongodb.net/projet_NodeJs?retryWrites=true',{useNewUrlParser: true});
+//const mongoose = require('mongoose');
+//mongoose.connect('mongodb+srv://RC:B4IgWhoqchuiTm3w@cluster0-uuws7.mongodb.net/projet_NodeJs?retryWrites=true',{useNewUrlParser: true});
 /////////////////////////////////////
+
+
 
 ////////////// login ////////////////
 app.get('/Login', function(req, res){
-	res.render('login');
-})
+	res.render('login', {badauth: false});
+});
 
-app.post('/Login', function(req, res){
-	//check if login and password are ok
-	res.cookie('login', req.body.login, { maxAge: 60*60*24*1000 });
-	res.redirect('/GameSelection');
-})
+app.post('/Login', function(req, res) {
+	//check if login and password are ok using database
+	if (true) {
+		req.session.user = user;
+		res.redirect('/secure/GameSelection');
+	} else {
+		res.render('login', {badauth: true});
+	}
+});
 
 
 /////////////subscription//////////
 app.get('/Subscription', function(req, res){
 	res.render('subscription', {user: user});
-})
+});
 
 
 ///////////// game selection //////////////
-app.get('/GameSelection', function(req, res){
+app.get('/secure/GameSelection', function(req, res){
 	res.render('gameList', {gameList: gameList});
-})
-
-app.get('/GameSelection', function(req, res){
-	res.render('gameList', {gameList: gameList});
-})
+});
 
 
 //////////// game random ///////////////
-app.get('/Random', function(req, res){
-	res.render('random', {user: user});
-})
+app.get('/secure/Random', function(req, res){
+	res.render('random', {user: req.session.user});
+});
 
-app.post('/Random', function(req, res){
+app.post('/secure/Random', function(req, res){
 	score = req.body.score;
-	if(user.highscore < score) {
-		user.highscore = score;
+	if(req.session.user.highscore < score) {
+		req.session.user.highscore = score;
 	}
-	res.render('random', {user: user});
-})
+	res.render('random', {user: req.session.user});
+});
 
 
 //////////// game space shooter ///////////
-app.get('/SpaceGame', function(req, res){		//ne marche pas => trouve pas les sources
-	res.render('spaceGame', {user: user});
+app.get('/secure/SpaceGame', function(req, res){		//ne marche pas => trouve pas les sources
+	res.render('spaceGame', {user: req.session.user});
 })
 
 
