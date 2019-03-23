6a70b34c148074675d0bc3f7e52c905bcf95015e
Romane <rcastel@enssat.fr>
2019-03-23 11:43:10 +0100

    admin can delete users

--- index.js
+++ index.js
@@ -145,8 +145,26 @@ app.get('/secure/GameSelection', function(req, res){
 
 /////////////admin users gestion //////////
 app.get('/secure/Admin', function(req, res){
-	res.render('admin');
+	User.find().then((users) =>{
+		//console.log(users);
+		res.render('admin', {users});
+	});
 })
+
+app.post('/secure/Admin',function(req,res){
+	req.checkBody('login', 'Please insert a login.').notEmpty();
+	User.findOne({
+		login : req.body.login
+	}, function(err,user) {
+		if(!user){
+			res.redirect('/secure/Admin');
+		}
+		else{
+			user.remove();
+			res.redirect('/secure/Admin')
+		}
+	})
+});
 
 
 //////////// game space shooter N°0///////////
